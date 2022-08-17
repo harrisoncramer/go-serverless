ea4d5f4741122916e875b10a51ed6322b990d771
Harrison Cramer <harrisoncramer@gmail.com>
2022-08-16 22:07:52 -0400

    Changed table name

--- cmd/main.go
+++ cmd/main.go
@@ -32,7 +32,7 @@ func main() {
 	lambda.Start(handler)
 }
 
-const tableName = "LambdaInGoUser"
+const tableName = "go-serverless"
 
 func handler(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	switch req.HTTPMethod {
